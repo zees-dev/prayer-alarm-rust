dd74f191daa7a70ba63ab57519ffa547fbc61744
zees-dev <dev.786zshan@gmail.com>
2022-11-21 22:38:23 +1300

    updated deps - using islam instead of salah

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,19 @@
-use chrono::Utc;
+use chrono::{Date, Local, Timelike, Utc};
+use islam::pray::{Config, Location, Prayer, PrayerTimes};
 use rodio::{Decoder, Sink};
-use salah::{Coordinates, Date, Parameters, Prayer, PrayerTimes};
 use std::fs::File;
 use std::io::BufReader;
+use std::ops::Sub;
 
 #[derive(Clone)]
 pub struct AdhanService<'a> {
-    pub coords: &'a Coordinates,
-    pub config: &'a Parameters,
+    pub coords: &'a (f32, f32), // latitude, longitude
+    pub config: &'a Config,
     pub sink: &'a Sink,
 }
 
 impl<'a> AdhanService<'a> {
-    pub fn new(coords: &'a Coordinates, config: &'a Parameters, sink: &'a Sink) -> Self {
+    pub fn new(coords: &'a (f32, f32), config: &'a Config, sink: &'a Sink) -> Self {
         Self {
             coords,
             config,
@@ -20,53 +21,75 @@ impl<'a> AdhanService<'a> {
         }
     }
 
-    pub fn init_prayer_alarm(self) {
-        let date_utc: Date<Utc> = chrono::Utc::now().date();
-        let prayer_times = PrayerTimes::new(date_utc, *self.coords, *self.config);
+    pub fn get_prayer_times(&self) -> PrayerTimes {
+        let date: Date<Local> = chrono::Local::today();
 
-        // if next prayer time is not fajr for tomorrow, then process all todays prayers
-        while prayer_times.next() != Prayer::FajrTomorrow {
-            let (next_prayer, (hours, mins)) = match prayer_times.next() {
-                Prayer::Fajr | Prayer::Dhuhr | Prayer::Asr | Prayer::Maghrib | Prayer::Isha => {
+        // get timezone from via calculating the offset from UTC
+        let timezone = chrono::Local::now().offset().local_minus_utc() / 3600;
+
+        PrayerTimes::new(
+            date,
+            Location::new(self.coords.0, self.coords.1, timezone),
+            *self.config,
+        )
+    }
+
+    pub fn init_prayer_alarm(&self) {
+        let today = chrono::Local::today();
+        println!("Today is {:#?}", today);
+
+        let prayer_times = self.get_prayer_times();
+        // println!("Prayer times {:#?}", prayer_times);
+        println!("Prayer times current {:#?}", prayer_times.current());
+        println!("Prayer times for {:#?}", prayer_times.next());
+
+        println!("now time {:#?}", chrono::Local::now().time().hour());
+
+        while today == chrono::Local::today() {
+            // if next prayer time is not fajr for tomorrow, then process all todays prayers
+            let (next_prayer, (hours, mins)) = match prayer_times.current() {
+                Prayer::Sherook | Prayer::Dohr | Prayer::Asr | Prayer::Maghreb => {
                     (prayer_times.next(), prayer_times.time_remaining()) // time till next
                 }
-                // current prayer is Fajr, skip Sunrise
-                Prayer::Sunrise => (
-                    Prayer::Dhuhr,
-                    get_time_till_prayer(&prayer_times, &Prayer::Dhuhr),
+                // skip Sunrise/Sherook
+                Prayer::Fajr => (
+                    Prayer::Dohr,
+                    get_time_till_prayer(&prayer_times, &Prayer::Dohr),
                 ),
-                // current prayer is Isha, skip Qiyam
-                Prayer::Qiyam | Prayer::FajrTomorrow => (
-                    Prayer::FajrTomorrow,
-                    get_time_till_prayer(&prayer_times, &Prayer::FajrTomorrow),
-                ),
+                // current prayer is isha, calculate time till fajr for tomorrow
+                Prayer::Ishaa => {
+                    let duration_secs = if chrono::Local::now().time().hour() < 12 {
+                        // before 12pm, this is isha midnight time, calculate time till fajr for today
+                        prayer_times.fajr.sub(chrono::Local::now()).num_seconds()
+                    } else {
+                        // after 12pm (before 12am next day), calculate time till fajr for tomorrow
+                        prayer_times
+                            .fajr_tomorrow
+                            .sub(chrono::Local::now())
+                            .num_seconds()
+                    };
+                    let hours = duration_secs / 3600;
+                    let mins = (duration_secs % 3600) / 60;
+                    (prayer_times.next(), (hours as u32, mins as u32))
+                }
             };
 
-            // convert hours and mins to secs
-            let secs_till_next_prayer = hours * 3600 + mins * 60;
             println!(
-                "Time till {:?} prayer: {:?}s...",
+                "Currently {:?}; time till {:?} prayer: {:?}:{:?}:00...",
+                prayer_times.current().name(),
                 next_prayer.name(),
-                secs_till_next_prayer
+                hours,
+                mins,
             );
-            std::thread::sleep(std::time::Duration::from_secs(secs_till_next_prayer as u64));
+            let secs_till_prayer = (hours * 60 + mins) * 60;
+            std::thread::sleep(std::time::Duration::from_secs(secs_till_prayer as u64));
             println!(
                 "Playing adhan {:?} at {:?}...",
-                prayer_times.current().name(),
+                next_prayer.name(),
                 chrono::Local::now().time().format("%-l:%M %p").to_string()
             );
             self.play_adhan(&next_prayer);
         }
-
-        // println!(
-        //     "{}: {}",
-        //     Prayer::Fajr.name(),
-        //     prayer_times
-        //         .time(Prayer::Fajr)
-        //         .with_timezone(&chrono::Local)
-        //         .format("%-l:%M %p")
-        //         .to_string()
-        // );
 
         // call function again to process next day (since next prayer is FajrTomorrow)
         self.init_prayer_alarm();
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use islam::pray::{Config, Madhab, Method};
 use rodio::{OutputStream, Sink};
-use salah::{Configuration, Coordinates, Madhab, Method};
 use std::sync::Arc;
 
 fn main() {
@@ -17,8 +17,8 @@ fn main() {
     // let params = prayer_alarm::structs::Params::new("Auckland", "NewZealand");
 
     let service = prayer_alarm::AdhanService {
-        coords: &Coordinates::new(-36.8501, 174.7645),
-        config: &Configuration::with(Method::MuslimWorldLeague, Madhab::Hanafi),
+        coords: &(-36.8501, 174.764),
+        config: &Config::new().with(Method::MuslimWorldLeague, Madhab::Hanafi),
         sink: &sink,
     };
     service.init_prayer_alarm();
